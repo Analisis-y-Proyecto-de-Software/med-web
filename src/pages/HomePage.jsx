5f50f230d448ae7fef33232355de9a45d271be81
deivymendoza <deivy.santiago@amarusuite.com>
2026-03-07 13:01:50 -0500

    comnporbando despliege en vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 function HomePage() {
-  return <main className="min-h-screen bg-white" />
+  return <main className="min-h-screen bg-white"> 
+  <h1>Welcome to the Home Page</h1>
+  </main>
 }
 
 export default HomePage
